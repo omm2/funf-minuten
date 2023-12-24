7e75246655ffee42a9b9c011eddd87d22b990eac
junheng.zhang <junheng.zhang@uni-rostock.de>
2023-12-24 18:06:31 +0100

    first word inserted into db

--- config.py
+++ config.py
@@ -4,11 +4,13 @@ import os
 class Config:
     # SECRET_KEY = os.environ["SECRET_KEY"]
 
+    SECRET_KEY = '+?sSoPOLu?8vRCzoq`K|KKOYv4[br"V6[8?Jc|{xj?`l$}X&^KPC#W@82=2zi`9'
     basedir = os.path.abspath(os.path.dirname(__file__))
     # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "bluefriday.db")
-    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
-
-    # SQLALCHEMY_DATABASE_URI = "mysql://username:password@host:port/database_name"
+    # SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://funfminuten:funfminuten@db4free.net:3306/funfminuten"
+    )
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://baslsb:bfbaslsb2023@baslsb.mysql.pythonanywhere-services.com/baslsb$default"
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_SAMESITE = "None"
